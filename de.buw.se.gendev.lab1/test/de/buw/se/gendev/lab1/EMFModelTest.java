8e924b41a597e79616339d8c07d3297fffee7598
Jan Oliver Ringert <jan.ringert@uni-weimar.de>
2022-04-29 15:18:54 +0200

    renamed with valid package name

---
+++ de.buw.se.gendev.lab1/test/de/buw/se/gendev/lab1/EMFModelTest.java
@@ -0,0 +1,14 @@
+package de.buw.se.gendev.lab1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EMFModelTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
